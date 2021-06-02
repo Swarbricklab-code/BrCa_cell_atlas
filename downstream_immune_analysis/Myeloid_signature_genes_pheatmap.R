29b18a673fb18d79126661c95c4d1a8ead80cfbf
gAleryani <38425671+gAleryani@users.noreply.github.com>
2021-06-02 20:19:57 +1000

    Update Myeloid_signature_genes_pheatmap.R

--- downstream_immune_analysis/Myeloid_signature_genes_pheatmap.R
+++ downstream_immune_analysis/Myeloid_signature_genes_pheatmap.R
@@ -8,6 +8,81 @@ library(pheatmap)
 
 options(scipen=10000)
 
+#Function to add flag for pheatmap (genes of interest show only)
+add.flag <- function(pheatmap,
+                     kept.labels,
+                     repel.degree) {
+
+  # repel.degree = number within [0, 1], which controls how much
+  #                space to allocate for repelling labels.
+  ## repel.degree = 0: spread out labels over existing range of kept labels
+  ## repel.degree = 1: spread out labels over the full y-axis
+
+  heatmap <- pheatmap$gtable
+
+  new.label <- heatmap$grobs[[which(heatmap$layout$name == "row_names")]]
+
+  # keep only labels in kept.labels, replace the rest with ""
+  new.label$label <- ifelse(new.label$label %in% kept.labels,
+                            new.label$label, "")
+
+  # calculate evenly spaced out y-axis positions
+  repelled.y <- function(d, d.select, k = repel.degree){
+    # d = vector of distances for labels
+    # d.select = vector of T/F for which labels are significant
+
+    # recursive function to get current label positions
+    # (note the unit is "npc" for all components of each distance)
+    strip.npc <- function(dd){
+      if(!"unit.arithmetic" %in% class(dd)) {
+        return(as.numeric(dd))
+      }
+
+      d1 <- strip.npc(dd$arg1)
+      d2 <- strip.npc(dd$arg2)
+      fn <- dd$fname
+      return(lazyeval::lazy_eval(paste(d1, fn, d2)))
+    }
+
+    full.range <- sapply(seq_along(d), function(i) strip.npc(d[i]))
+    selected.range <- sapply(seq_along(d[d.select]), function(i) strip.npc(d[d.select][i]))
+
+    return(unit(seq(from = max(selected.range) + k*(max(full.range) - max(selected.range)),
+                    to = min(selected.range) - k*(min(selected.range) - min(full.range)),
+                    length.out = sum(d.select)),
+                "npc"))
+  }
+  new.y.positions <- repelled.y(new.label$y,
+                                d.select = new.label$label != "")
+  new.flag <- segmentsGrob(x0 = new.label$x,
+                           x1 = new.label$x + unit(0.15, "npc"),
+                           y0 = new.label$y[new.label$label != ""],
+                           y1 = new.y.positions)
+
+  # shift position for selected labels
+  new.label$x <- new.label$x + unit(0.2, "npc")
+  new.label$y[new.label$label != ""] <- new.y.positions
+
+  # add flag to heatmap
+  heatmap <- gtable::gtable_add_grob(x = heatmap,
+                                   grobs = new.flag,
+                                   t = 4,
+                                   l = 4
+  )
+
+  # replace label positions in heatmap
+  heatmap$grobs[[which(heatmap$layout$name == "row_names")]] <- new.label
+
+  # plot result
+  grid.newpage()
+  grid.draw(heatmap)
+
+  # return a copy of the heatmap invisibly
+  invisible(heatmap)
+}
+
+
+
 #Set wd
 gen_dir <- "/Single_cell/breast_atlas/"
 setwd(gen_dir)
@@ -34,14 +109,9 @@ y_newdata <- data.frame(dutertre_gene_top20[c(1,7)])
 
 #Other studies gene list take top 20
 
-#Duetere gene list
-myeloid_multiple_gene_lists <- (read.table(paste0("references/myeloid_multiple_study_gene_signature.csv"), header = T, sep = ","))
-multiple_myeloid_top20 <- myeloid_multiple_gene_lists %>%
-  group_by(cluster) %>%
-  top_n(n = 20, wt = gene)
-
-#take info of interest
-x_newdata <- data.frame(multiple_myeloid_top20)
+#Other studies already taken top 20 gene lists
+myeloid_multiple_gene_lists <- (read.table(paste0("references/Myeloid_multiple_study_gene_signature.csv"), header = T, sep = ","))
+x_newdata <- data.frame(myeloid_multiple_gene_lists)
 
 #merge two gene of interests and prepare annotations
 anno_data <- rbind(x_newdata,y_newdata)
